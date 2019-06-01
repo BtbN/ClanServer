6a8dc3f96b71173c72fea8db00930602618b4898
BtbN <btbn@btbn.de>
2019-06-01 17:18:06 +0200

    Implement get_pdata

---
+++ ClanServer/Controllers/L44/Lobby.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+using eAmuseCore.KBinXML;
+
+using ClanServer.Routing;
+
+namespace ClanServer.Controllers.L44
+{
+    [ApiController, Route("L44")]
+    public class LobbyController : ControllerBase
+    {
+        [HttpPost, Route("8"), XrpcCall("lobby.check")]
+        public ActionResult<EamuseXrpcData> Check([FromBody] EamuseXrpcData data)
+        {
+            //TODO
+            data.Document = new XDocument(new XElement("response", new XElement("lobby")));
+
+            return data;
+        }
+
+        [HttpPost, Route("8"), XrpcCall("lobby.entry")]
+        public ActionResult<EamuseXrpcData> Entry([FromBody] EamuseXrpcData data)
+        {
+            //TODO
+            data.Document = new XDocument(new XElement("response", new XElement("lobby")));
+
+            return data;
+        }
+    }
+}
