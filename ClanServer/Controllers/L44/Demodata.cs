fec9a1174c4b5da0052260734cb7226b9402939e
BtbN <btbn@btbn.de>
2019-06-03 22:12:54 +0200

    Implement Shim get_hitchart

--- ClanServer/Controllers/L44/Demodata.cs
+++ ClanServer/Controllers/L44/Demodata.cs
@@ -34,18 +34,49 @@ namespace ClanServer.Controllers.L44
             return data;
         }
 
-        [HttpPost, Route("8"), XrpcCall("demodata.get_hitchart")]
-        public ActionResult<EamuseXrpcData> GetHitchart([FromBody] EamuseXrpcData data)
+        [HttpPost, Route("8"), XrpcCall("demodata.get_jbox_list")]
+        public ActionResult<EamuseXrpcData> GetJboxList([FromBody] EamuseXrpcData data)
         {
             data.Document = new XDocument(new XElement("response", new XElement("demodata")));
 
             return data;
         }
 
-        [HttpPost, Route("8"), XrpcCall("demodata.get_jbox_list")]
-        public ActionResult<EamuseXrpcData> GetJboxList([FromBody] EamuseXrpcData data)
+        [HttpPost, Route("8"), XrpcCall("demodata.get_hitchart")]
+        public ActionResult<EamuseXrpcData> GetHitchart([FromBody] EamuseXrpcData data)
         {
-            data.Document = new XDocument(new XElement("response", new XElement("demodata")));
+            int[] hitChart = new int[]
+            {
+                70000110,
+                80000016,
+                60000080,
+                50000071,
+                60000115,
+                30000004,
+                70000079,
+                50000113,
+                80000086,
+                70000033
+            };
+
+            XElement orgElem = new XElement("hitchart_org", new XAttribute("count", hitChart.Length));
+
+            for (short i = 0; i < hitChart.Length; ++i)
+            {
+                orgElem.Add(new XElement("rankdata",
+                    new KS32("music_id", hitChart[i]),
+                    new KS16("rank", i),
+                    new KS16("prev", i)
+                ));
+            }
+
+            data.Document = new XDocument(new XElement("response", new XElement("demodata",
+                new XElement("data",
+                    new KStr("update", "1"),
+                    new XElement("hitchart_lic", new XAttribute("count", 0)),
+                    orgElem
+                )
+            )));
 
             return data;
         }
