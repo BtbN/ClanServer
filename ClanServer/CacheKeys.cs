09307826e86d8f26f86394e36ef5387ce8eb7355
BtbN <btbn@btbn.de>
2019-06-06 18:27:55 +0200

    Cache recommended songs so they persist one credit

---
+++ ClanServer/CacheKeys.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClanServer
+{
+    public static class CacheKeys
+    {
+        public static string GetRecommendedSongsKey(int jid) => $"_RecommendedSongs_{jid}";
+    }
+}
