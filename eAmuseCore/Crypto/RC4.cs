d0d1c362ed1ec8234d5b8286ac9f1968f7bea142
BtbN <btbn@btbn.de>
2019-06-14 16:58:06 +0200

    De/Encrypt in-place to enhance performance

--- eAmuseCore/Crypto/RC4.cs
+++ eAmuseCore/Crypto/RC4.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Security.Cryptography;
 using System.Linq;
 using System;
@@ -11,16 +11,16 @@ namespace eAmuseCore.Crypto
             new byte[]{ 0x69, 0xd7, 0x46, 0x27, 0xd9, 0x85, 0xee, 0x21, 0x87, 0x16, 0x15, 0x70, 0xd0,
                         0x8d, 0x93, 0xb1, 0x24, 0x55, 0x03, 0x5b, 0x6d, 0xf0, 0xd8, 0x20, 0x5d, 0xf5 };
 
-        public static IEnumerable<byte> ApplyEAmuseInfo(string info, IEnumerable<byte> data)
+        public static void ApplyEAmuseInfo(string info, byte[] data)
         {
             if (!info.StartsWith("1-") || info.Count(c => c == '-') != 2 || info.Length != 15)
                 throw new ArgumentException("Unknown E-Amuse-Info format.", "info");
             info = info.Substring(2).Replace("-", "");
             byte[] key = Enumerable.Range(0, info.Length / 2).Select(i => Convert.ToByte(info.Substring(i * 2, 2), 16)).ToArray();
-            return ApplyEAmuse(key, data);
+            ApplyEAmuse(key, data);
         }
 
-        public static IEnumerable<byte> ApplyEAmuse(byte[] key, IEnumerable<byte> data)
+        public static void ApplyEAmuse(byte[] key, byte[] data)
         {
             if (key.Length != 6)
                 throw new ArgumentException("Key length has to be exactly 6 bytes.", "key");
@@ -28,13 +28,13 @@ namespace eAmuseCore.Crypto
             using (MD5 md5 = MD5.Create())
             {
                 byte[] realKey = md5.ComputeHash(key.Concat(konamiCode).ToArray());
-                return EncryptOutput(realKey, data);
+                EncryptOutput(realKey, data);
             }
         }
 
-        public static IEnumerable<byte> Apply(byte[] key, IEnumerable<byte> data)
+        public static void Apply(byte[] key, byte[] data)
         {
-            return EncryptOutput(key, data);
+            EncryptOutput(key, data);
         }
 
         private static byte[] EncryptInitalize(byte[] key)
@@ -50,21 +50,21 @@ namespace eAmuseCore.Crypto
             return s;
         }
 
-        private static IEnumerable<byte> EncryptOutput(byte[] key, IEnumerable<byte> data)
+        private static void EncryptOutput(byte[] key, byte[] data)
         {
             byte[] s = EncryptInitalize(key);
 
             uint i = 0;
             uint j = 0;
 
-            foreach (byte b in data)
+            for (int k = 0; k < data.Length; ++k)
             {
                 i = (i + 1) & 0xff;
                 j = (j + s[i]) & 0xff;
 
                 Swap(s, i, j);
 
-                yield return (byte)(b ^ s[(s[i] + s[j]) & 0xff]);
+                data[k] ^= s[(s[i] + s[j]) & 0xff];
             };
         }
 
